ae3d16c9e28f8c373c19fe358837867beffd4758
Larry <larry@My-Mac.local>
2021-07-25 17:23:56 -0500

    Developer Bootcamp

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,6 +15,7 @@ require("./tasks/block-number")
 require("./tasks/random-number-consumer")
 require("./tasks/price-consumer")
 require("./tasks/api-consumer")
+require('solidity-coverage')
 
 
 require('dotenv').config()
@@ -32,18 +33,18 @@ module.exports = {
     networks: {
         hardhat: {
             // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
-            // }
+             forking: {
+               url: MAINNET_RPC_URL
+             }
         },
         localhost: {
         },
         kovan: {
             url: KOVAN_RPC_URL,
-            // accounts: [PRIVATE_KEY],
-            accounts: {
-                mnemonic: MNEMONIC,
-            },
+             accounts: [PRIVATE_KEY],
+            //accounts: {
+               // mnemonic: MNEMONIC,
+           // },
             saveDeployments: true,
         },
         rinkeby: {
